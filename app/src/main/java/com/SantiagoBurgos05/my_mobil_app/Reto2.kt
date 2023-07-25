020b48d08dabe58ebb6eed4fb530a29c6ad22055
SantiagoBurgos05 <santiagobuitragoburgos54@gmail.com>
2023-07-25 18:05:17 -0500

    Se inicializó el proyecto kotlin 2

---
+++ app/src/main/java/com/SantiagoBurgos05/my_mobil_app/Reto2.kt
@@ -0,0 +1,36 @@
+package com.SantiagoBurgos05.my_app_movil
+
+class Reto2 {
+}
+fun main(){
+    var con: String
+    var tot: Float
+    var pro: Float
+    var cant: Float
+    var res: Float=0F
+    do {
+        println("Ingrese el valor del producto")
+        pro= readLine()!!.toFloat()
+        println("Cantidad del producto")
+        cant= readLine()!!.toFloat()
+        println("Desea digitar otro valor?")
+        con = readLine()!!.toString()
+        tot=pro*cant
+        res= res+tot
+    } while (con == "Si" || con == "SI" || con == "sI" || con == "si")
+
+    var bola: Int= (1..4).random()
+    if (res>50000 && bola==1) {
+        println("Su valor a pagar es ${res} \n Sacaste la bola roja obtienes 10% de descuento en el valor de tu compra \n  Tu valor a pagar es ${res-(res*0.10)}")
+
+    }else if(res>50000 && bola==2){
+        println("Su valor a pagar es ${res} \n Sacaste la bola azul obtienes 30% de descuento en el valor de tu compra \n Tu valor a pagar es  ${res-(res*0.30)}")
+    }
+    else if(res>50000 && bola==3){
+        println("Su valor a pagar es ${res} \n Sacaste la bola amarilla obtienes 50% de descuento en el valor de tu compra \n Tu valor a pagar es  ${res-(res*0.50)}")
+    }
+    else if(res>50000 && bola==4){
+        println("Su valor a pagar es ${res} \n Sacaste la bola blanca \n ¡FELICITACIONES, TE LLEVAS TU COMPRA TOTALMENTE GRATIS!!!")
+    }else
+        println("Tu compra no ha sido beneficiada \n Su valor a pagar es ${res}")
+}
---
+++ app/src/main/java/com/SantiagoBurgos05/my_mobil_app/Reto3.kt
@@ -0,0 +1,61 @@
+package com.SantiagoBurgos05.my_app_movil
+
+class Reto3 {
+}
+fun main(){
+    println("Bienvenidos a automoviles Jeff")
+    var i: String="si"
+    var lista = mutableListOf("Mustang GT","Supra","Rayo McQueen")
+    do {
+        println("Que deseas hacer el dia de hoy?")
+        println("\nIngrese el numero de la opcion que desea\n1.Ingresar nuevos autos || 2.Mostrar los autos de la lista || 3.Buscar un carro en particular \n 4.Modificar un vehiculo existtente || 5.Eliminar un vehiculo existente")
+        var opt: Int= readLine()!!.toInt()
+
+        if (opt==1){
+            println("Ingresa el nombre del auto que deseas agregar")
+            var add: String= readLine()!!.toString()
+            lista.add(add)
+            println(lista)
+
+        }else if (opt==2){
+            println("Esta es la lista actual de los autos: \n${lista}")
+
+        }else if (opt==3){
+            println("Ingrese el nombre del carro que desea consultar")
+            var buscar:String= readLine()!!.toString()
+            val buscare = lista.any {carro -> carro.equals(buscar, ignoreCase = true)}
+            if (buscare==true) {
+                println("El carro que usted buscó fue: ${buscar} ")
+                //println(lista.find { it == buscar })
+            }else{
+                println("El carro ingresado no existe, MMG")
+            }
+        //Pendiente
+
+        }else if (opt==4){
+            println("Ingrese el carro que desea modificar: ")
+            var carro:String= readLine()!!.toString()
+
+            println("Ingrese el cambio que desea realizar: ")
+            var modifi:String= readLine()!!.toString()
+
+            var cambio=lista.indexOf(carro)
+            lista[cambio]=modifi
+            println("La lista modificada quedó asi: \n${lista}")
+
+        }else if (opt==5){
+            println("Ingrese el nombre del carro que desea eliminar")
+            var delete: String= readLine()!!.toString()
+            lista.remove(delete)
+            println("El carro que usted eliminó fue: ${delete} y la lista quedó asi: \n${lista}")
+
+        }else{
+            println("Tu desición no está dentro de las posibilidades. Cacorro!")
+        }
+
+        println("Desea realizar otra operacion?")
+        i= readLine()!!.toString()
+
+    }while (i == "Si" || i == "SI" || i == "sI" || i == "si")
+    println("Gracias.. Pico y Chao")
+}
